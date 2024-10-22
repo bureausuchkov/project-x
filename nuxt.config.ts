4ec51cd411861b76f66beff8799e24c8519cfaee
artemstolarenko <artemstolyarenko@yandex.ru>
2024-10-22 12:50:05 +0500

    Добавил скрипт

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,18 @@ export default defineNuxtConfig({
       title: "Харизмы",
 
       link: [{ rel: "shortcut icon", href: "favicon.ico" }],
+      script: [
+        {
+          innerHTML: `(function(w, d, s, h, id) {
+            w.roistatProjectId = id; w.roistatHost = h;
+            var p = d.location.protocol == "https:" ? "https://" : "http://";
+            var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
+            var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+            })(window, document, 'script', 'cloud.roistat.com', '69166936994c8d5dc38b7f63b341a71b');
+          `,
+          type: "text/javascript",
+        }
+      ],
       meta: [{ hid: "description", name: "description", content: "Харизмы" }],
     },
   },
